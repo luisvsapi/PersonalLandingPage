0ee555ee1b201635b34888a8be81992428d75432
Luis Villamar <luisvsapi@gmail.com>
2023-05-10 03:55:33 -0500

    [ADD] Agregando particulas a seccion inicial

--- src/app/curriculum/body/home/home.component.ts
+++ src/app/curriculum/body/home/home.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  myScriptElement!: HTMLScriptElement;
+
   constructor() { }
 
   ngOnInit(): void {
+    // Lo llamo aqui porque en este punto ya existe el div que quiero que contenga las particulas
+    this.myScriptElement = document.createElement("script");
+    this.myScriptElement.src = "assets/scripts/app.js";
+    document.body.appendChild(this.myScriptElement);
   }
 
 }
